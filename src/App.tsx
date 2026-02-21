21c87788570f6f9319d8b80522adf180291c1d3e
Ashwani Kushwaha <ashwanik346981@gmail.com>
2026-02-21 12:54:32 +0530

    update all module page

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
         <Route path="/" element={<AppLayout />}>
           <Route index element={<HomePage />} />
           <Route path="docs" element={<DocumentationPage />} />
-          <Route path="Docs" element={<DocumentationPage />} />
+          <Route path="Documentation" element={<DocumentationPage />} />
           <Route path="modules/user-access" element={<UserAccessPage />} />
           <Route path="modules/complaint-intake" element={<ComplaintIntakePage />} />
           <Route path="modules/assignment" element={<AssignmentPage />} />
